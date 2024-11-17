f401f9e65b90f09dda96f16e877fdb8c408a6f1a
liana <lylaent@gmail.com>
2024-11-17 14:46:44 +0330

    edite IconBox component

--- enteshari-week11-1/src/components/common/ui/icon-box/IconBox.tsx
+++ enteshari-week11-1/src/components/common/ui/icon-box/IconBox.tsx
@@ -7,24 +7,54 @@ interface Props {
     size?: number;
     link?: string;
     title?: string;
-    hiddeTitleOnMobile?:boolean;
-    badge?:number;
+    hiddeTitleOnMobile?: boolean;
+    badge?: number;
+    titleClassName?: string;
+    path?: number;
+    linkClass?: string;
 }
 
-export function IconBox({icon, size = 22, link, title, hiddeTitleOnMobile = false, badge=0}: Props) {
+export function IconBox({
+                            icon,
+                            size = 22,
+                            link,
+                            title,
+                            hiddeTitleOnMobile = false,
+                            badge = 0,
+                            titleClassName = '',
+                            path,
+                            linkClass = ''
+                        }: Props) {
+    let span = [];
+    for (let i = 1; i <= path; i++) {
+        span.push(<span className={`path${i}`}></span>)
+
+    }
+
     return (
-        <>
+        <Link href="#" className={linkClass}>
             {
                 badge ?
                     <div className="relative">
-                        <span className="absolute -top-[10px] -right-[10px] w-[20px] h-[20px] bg-green-200 rounded-full flex justify-center items-center text-white text-xsmall">{badge}</span>
-                        <Link href="#"><i className={` ${icon} text-[${size}px]`}></i></Link>
+                        <span
+                            className="absolute -top-[10px] -right-[10px] w-[20px] h-[20px] bg-green-200 rounded-full flex justify-center items-center text-white text-xsmall">{badge}</span>
+                            <i className={` ${icon} text-[${size}px]`}>
+                                {
+                                    path && span
+                                }
+
+                            </i>
+
                     </div>
                     :
-                    <Link href={link ?? '#'}> <i className={` ${icon} text-[${size}px]`}></i></Link>
+                        <i className={` ${icon} text-[${size}px]`}>
+                            {path && span}
+                        </i>
             }
             {
-                title && <div className={`${hiddeTitleOnMobile ? 'hidden xl:inline-block':'inline-block'} text-medium text-gray-500 font-lato`}>{title}</div>
+                title && <div
+                    className={`${hiddeTitleOnMobile ? 'hidden xl:inline-block' : 'inline-block'} ${titleClassName}`}>{title}</div>
             }
-            </>);}
+        </Link>);
+}
 
