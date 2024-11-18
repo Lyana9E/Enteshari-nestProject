d322338e8aeb6f97e6fe1090d2bde95faa86b80a
liana <lylaent@gmail.com>
2024-11-18 14:44:14 +0330

    create mock for menu

---
+++ enteshari-week11-1/src/mocks/MenuMock.ts
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export const MenuMock=[
+    {
+        title:"Hot Deals",
+        icon:"icon-flame",
+        link:"#",
+    },
+        {
+            title:"Home",
+            icon:"",
+            link:"#",
+        },
+        {
+            title:"Food",
+            icon:"",
+            link:"#",
+        },
+        {
+            title:"Vegetables",
+            icon:"",
+            link:"#",
+        },
+]
+
