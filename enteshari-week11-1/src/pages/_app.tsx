cc97b7eafe5e8e1cb94156bd7faf3f5bdccffc8b
liana <lylaent@gmail.com>
2024-11-19 10:26:07 +0330

    add fonts from next/font package

--- enteshari-week11-1/src/pages/_app.tsx
+++ enteshari-week11-1/src/pages/_app.tsx
@@ -2,11 +2,26 @@ import "@/styles/globals.css";
 import "@/styles/icon.css";
 import type {AppProps} from "next/app";
 import {Layout} from "@/components";
+import {Lato, Quicksand} from "next/font/google";
 
+
+const quicksand = Quicksand({
+    subsets: ['latin']
+})
+const lato = Lato({
+    weight:['100', '300'],
+    subsets:['latin'],
+    variable : '--font-lato',
+})
 export default function App({Component, pageProps}: AppProps) {
+
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <style jsx global>{`
+            `}</style>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     )
 }
--- enteshari-week11-1/src/pages/index.tsx
+++ enteshari-week11-1/src/pages/index.tsx
@@ -1,9 +1,18 @@
+import {Banner, Section} from "@/components";
 
 
 export default function Home() {
   return (
   <>
-    this is index
+    <Section>
+      <Banner
+          title={'Don’t miss amazing grocery deals'}
+          subTitle={'Sign up for the daily newsletter'}
+          bgImage={'/assets/images/banner_bg.png'}
+          Image={'/assets/images/fresh-apples.png'}
+
+      />
+    </Section>
   </>
   );
 }
