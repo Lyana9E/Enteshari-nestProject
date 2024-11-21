7d056892460a8b930276855ed4b4c5ed420e3924
liana <lylaent@gmail.com>
2024-11-21 13:56:48 +0330

    create mock for popular fruit and product

---
+++ enteshari-week11-1/src/mocks/PopularProductsMock.ts
@@ -0,0 +1,66 @@
+export const PopularProductsMock = [
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+        imgSrc: '/assets/images/products/mango.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        label: 'Hot'
+    },
+    {
+        title: 'All Natural Italian-Style Chicken Meatballs',
+        imgSrc: '/assets/images/products/corn.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        label: 'Hot'
+    },
+    {
+        title: 'Angie’s Boomchickapop Sweet & Salty Kettle Corn',
+        imgSrc: '/assets/images/products/tanjerin.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+    },
+    {
+        title: 'Foster Farms Takeout Crispy Classic Buffalo Wings',
+        imgSrc: '/assets/images/products/paper.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        label: 'Hot'
+    },
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+        imgSrc: '/assets/images/products/lemon.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        label: 'sale'
+    },
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+        imgSrc: '/assets/images/products/cucumber.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        label: 'Hot'
+    },
+]
