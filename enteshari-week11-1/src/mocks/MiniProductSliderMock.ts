f59b355e46e824c3bd850d55de9a58eaaf7519f6
liana <lylaent@gmail.com>
2024-11-21 13:53:27 +0330

    set mini product slider

---
+++ enteshari-week11-1/src/mocks/MiniProductSliderMock.ts
@@ -0,0 +1,20 @@
+export const MiniProductSliderMock = [
+    {
+        title: 'Everyday Fresh & Clean with Our Products',
+        imgSrc: '/assets/images/products/union.png',
+        link: '#',
+        bgColor: '#F0E8D5',
+    },
+    {
+        title: 'Everyday Fresh & Clean with Our Products',
+        imgSrc: '/assets/images/products/drink.png',
+        link: '#',
+        bgColor: '#F3E8E8',
+    },
+    {
+        title: 'The best Organic Products Online',
+        imgSrc: '/assets/images/products/fruit.png',
+        link: '#',
+        bgColor: '#E7EAF3',
+    },
+]
