31de5f5b4a4debc31a04ab24511b64887d5bf238
liana <lylaent@gmail.com>
2024-11-27 10:48:02 +0330

    create mock for best seller slider

---
+++ enteshari-week11-1/src/mocks/BestSellerSliderMock.ts
@@ -0,0 +1,78 @@
+export const BestSellersMock = [
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+        imgSrc: '/assets/images/products/mango.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 3.8,
+        label: 'Hot',
+        sold: 90,
+        total: 120
+    },
+    {
+        title: 'All Natural Italian-Style Chicken Meatballs',
+        imgSrc: '/assets/images/products/corn.png',
+        category: 'Hodo Foods',
+        rate: 1.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        label: 'Hot',
+        sold: 90,
+        total: 120
+    },
+    {
+        title: 'Angie’s Boomchickapop Sweet & Salty Kettle Corn',
+        imgSrc: '/assets/images/products/tanjerin.png',
+        category: 'Hodo Foods',
+        rate: 2.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        sold: 20,
+        total: 120
+    },
+    {
+        title: 'Foster Farms Takeout Crispy Classic Buffalo Wings',
+        imgSrc: '/assets/images/products/paper.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        label: 'Hot',
+        sold: 50,
+        total: 120
+    },
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+        imgSrc: '/assets/images/products/lemon.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        label: 'sale',
+        sold: 30,
+        total: 120
+    },
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+        imgSrc: '/assets/images/products/cucumber.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        label: 'Hot',
+        sold: 40,
+        total: 120
+    }
+]
