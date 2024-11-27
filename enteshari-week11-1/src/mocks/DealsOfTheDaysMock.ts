27049d2f46f65512679c599c3a7e1ba60fce3df5
liana <lylaent@gmail.com>
2024-11-27 15:03:04 +0330

    create mock for deals section

---
+++ enteshari-week11-1/src/mocks/DealsOfTheDaysMock.ts
@@ -0,0 +1,46 @@
+export const DealsOfTheDaysMock = [
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+        imgSrc: '/assets/images/deals/lettuce.png',
+        category: 'Hodo Foods',
+        rate: 1.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        dead_line: '2024-12-28 12:10:02'
+    },
+    {
+        title: 'Angie’s Boomchickapop Sweet & Salty Kettle Corn',
+        imgSrc: '/assets/images/deals/orange.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        dead_line: '2024-12-28 12:10:02'
+    },
+    {
+        title: 'Seeds of Change Organic Quinoa, Brown, & Red Rice',
+        imgSrc: '/assets/images/deals/blood-orange.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        dead_line: '2024-12-28 12:10:02'
+    },
+    {
+        title: 'Angie’s Boomchickapop Sweet & Salty Kettle Corn',
+        imgSrc: '/assets/images/deals/beans.png',
+        category: 'Hodo Foods',
+        rate: 4.0,
+        weight: 500,
+        unit: 'gram',
+        price: 3.50,
+        sale_price: 2.80,
+        dead_line: '2024-12-28 12:10:02'
+    },
+]
