ee378b3190c32b9a7395adaf5be243bb646914f6
liana <lylaent@gmail.com>
2025-01-20 10:46:28 +0330

    set live search

--- enteshari-week11-1/src/components/layouts/header/search-form/SearchForm.tsx
+++ enteshari-week11-1/src/components/layouts/header/search-form/SearchForm.tsx
@@ -1,64 +1,76 @@
 import {IconBox} from "@/components/common/ui/icon-box";
 import {useForm} from "react-hook-form";
-import {register} from "swiper/swiper-element";
 import {useMutation} from "@tanstack/react-query";
 import {getAllProductsApiCall} from "@/api/Product";
 import {useEffect, useState} from "react";
 import {EntityType} from "@/types";
 import {ProductType} from "@/types/api/Product";
+import useDebounce from "@/hooks/use-debounce";
+
 // TODO should implement search form
 
 interface Props {
-    inputClassName?:string;
+    inputClassName?: string;
 
 }
 
 interface formInput {
-    search_text:string;
+    search_text: string;
 
 }
 
-interface filterData{
-    title : {
-        $containsi  : string;
+interface filterData {
+    title: {
+        $containsi: string;
 
     }
 }
-export function SearchForm({inputClassName=''}:Props) {
 
-    const [resultData , setResultData]=useState<Array<EntityType<ProductType>>>([])
-  const {register,handleSubmit}=  useForm<formInput>()
+export function SearchForm({inputClassName = ''}: Props) {
 
-    const mutation = useMutation({mutationFn:(data:filterData)=>getAllProductsApiCall({filters:data})})
+    const [resultData, setResultData] = useState<Array<EntityType<ProductType>>>([])
+    const {register, handleSubmit,watch} = useForm<formInput>()
 
-    const onSubmit =(data:formInput)=>{
-        console.log(data)
+    const mutation = useMutation({mutationFn: (data: filterData) => getAllProductsApiCall({filters: data})})
+    const search_text = watch('search_text');
+
+    useEffect(() => {
+        if(search_text){
+            delay()
+        }else {
+            setResultData([])
+        }
+
+    }, [search_text]);
+    const onSubmit = (data: formInput) => {
+        if ( search_text.length <= 1 )
+            return;
         mutation.mutate({
 
-            title:{
+            title: {
                 '$containsi': data.search_text
             }
-        },{
-            onSuccess: (response)=>{
+        }, {
+            onSuccess: (response) => {
                 setResultData(response.data)
 
             }
-            }
-            )}
+        })
+    }
+    const delay = useDebounce(handleSubmit(onSubmit),1000)
 
 
-    useEffect(() => {
-        if (mutation.isSuccess) {
-            console.log('response', mutation.data);
-        }
-    }, [mutation.isSuccess, mutation.data]);
+
+
     return (
         <>
             <div className={'relative'}>
-                <form name="search-form" onSubmit={handleSubmit(onSubmit)} action="#" method="post" className="flex items-center">
-                    <input {...register('search_text')} type="text" placeholder="Search for items" className={`text-xsmall text-gray-400 border-gray-300 w-full  focus:outline-none ${inputClassName}`} />
+                <form name="search-form" onSubmit={handleSubmit(onSubmit)} action="#" method="post"
+                      className="flex items-center">
+                    <input autoComplete={"off"} {...register('search_text')} type="text" placeholder="Search for items"
+                           className={`text-xsmall text-gray-400 border-gray-300 w-full  focus:outline-none ${inputClassName}`}/>
                     <button type="submit">
-                        <IconBox icon={'icon-search'} />
+                        <IconBox icon={'icon-search'}/>
                     </button>
                 </form>
                 {
@@ -66,8 +78,9 @@ export function SearchForm({inputClassName=''}:Props) {
                     <div className={'absolute bg-white left-0 right-0 w-full top-14'}>
                         <ul>
                             {
-                                resultData.map((item:EntityType<ProductType> , index:number)=>{
-                                    return <li className={'p-4 hover:bg-green-200 hover:text-white cursor-pointer'}>{item.attributes.title}</li>
+                                resultData.map((item: EntityType<ProductType>, index: number) => {
+                                    return <li
+                                        className={'p-4 hover:bg-green-200 hover:text-white cursor-pointer'}>{item.attributes.title}</li>
                                 })
                             }
                         </ul>
